a3d26cff47d3690e64bb5f5375cb420cede2adbb
object-ions <moshikoatia@gmail.com>
2023-06-30 19:03:32 -0700

    add basic funcionality to the app

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,26 @@ import '../css/styles.css';
 console.log('loaded');
 
 //Get elements
-let firstCurrency = document.querySelector('#first-currency');
-let firstAmount = document.querySelector('#first-amount');
-let secondCurrency = document.querySelector('#second-currency');
-let secondAmount = document.querySelector('#second-amount');
+let firstCurrencyEl = document.querySelector('#first-currency');
+let firstAmountEl = document.querySelector('#first-amount');
 
+let secondCurrencyEl = document.querySelector('#second-currency');
+let secondAmountEl = document.querySelector('#second-amount');
+
+function calculate() {
+    let firstCurrencyVal = firstCurrencyEl.value;
+    let secondCurrencyVal = secondCurrencyEl.value;
+
+    console.log('currency1: ' + firstCurrencyVal);
+    console.log('currency2: ' + secondCurrencyVal);
+}
+
+
+//Event listener
+firstCurrencyEl.addEventListener('change', calculate);
+firstAmountEl.addEventListener('change', calculate);
+secondCurrencyEl.addEventListener('change', calculate);
+secondAmountEl.addEventListener('change', calculate);
+
+
+calculate();
