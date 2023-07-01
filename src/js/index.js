d08b81b5aeaa422c42910dd2d89a6d275e03345a
object-ions <moshikoatia@gmail.com>
2023-06-30 18:41:30 -0700

    starting to build the app w/o seperating buisness from UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,9 @@
 import '../css/styles.css';
-// import apiRequest from './apiRequest.js';
 
-// //Get elements
-// const displayDiv = document.querySelector('#display');
-// let errorElement = document.querySelector('#error');
+console.log('loaded');
 
-// //Defualt static vars
-// let amount = 0;
-// let firstCurrency = 'USD';
-// let secondCurrency = 'EUR';
-
-// function dataHandeler() {
-//     apiRequest.getRate()
-//         .then(function (response) {
-//             if (response.conversion_rates) {
-//                 // printResult(response, firstCurrency, secondCurrency);
-//                 console.log(response);
-//             } else {
-//                 // printError(response)
-//                 console.log('problem');
-//             }
-//         })
-// }
-
-// function printResult(response, firstCurrency, secondCurrency) {
-
-// }
-
-// function printError(error) {
-//     errorElement.innerText = `Error: ${error}`
-// }
+//Get elements
+let firstCurrency = document.querySelector('#first-currency');
+let firstAmount = document.querySelector('#first-amount');
+let secondCurrency = document.querySelector('#second-currency');
+let secondAmount = document.querySelector('#second-amount');
