d98c30c9818a84004ce601414e897250f53fcfa7
object-ions <moshikoatia@gmail.com>
2023-07-02 15:14:09 -0700

    solve a bug and add style

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
-import '../css/styles.css';
+import '../css/styles.css'
 
-function getRate(firstCurrencyVal, secondCurrencyVal) {
+function getRate(firstCurrencyVal, secondCurrencyVal, firstAmountVal) {
     fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${firstCurrencyVal}`)
         .then((response) => {
             if (!response.ok) {
@@ -11,17 +11,16 @@ function getRate(firstCurrencyVal, secondCurrencyVal) {
             }
         })
         .then((data) => {
-            // console.log(data);
             let firstRate = data.conversion_rates[firstCurrencyVal];
             let secondRate = data.conversion_rates[secondCurrencyVal];
-            console.log(firstCurrencyVal + ' ' + firstRate);
-            console.log(secondCurrencyVal + ' ' + secondRate);
 
-            manipDOM(data, firstCurrencyVal, firstRate, secondCurrencyVal, secondRate);
+            let convertedAmount = (firstAmountVal / firstRate) * secondRate;
+            secondAmountEl.value = convertedAmount.toFixed(2);
+
+            manipDOM(data, firstCurrencyVal, firstRate, secondCurrencyVal, secondRate, firstAmountVal, convertedAmount);
         })
 }
 
-//Get elements
 let firstAmountEl = document.querySelector('#first-amount');
 let firstCurrencyEl = document.querySelector('#first-currency');
 
@@ -33,31 +32,24 @@ function calculate() {
     let firstAmountVal = firstAmountEl.value;
 
     let secondCurrencyVal = secondCurrencyEl.value;
-    let secondAmountVal = secondAmountEl.value;
 
-    //show 'display' div with results
     document.getElementById('display').classList.remove('hidden');
 
-    getRate(firstCurrencyVal, secondCurrencyVal);
+    getRate(firstCurrencyVal, secondCurrencyVal, firstAmountVal);
 }
 
+function manipDOM(data, firstCurrencyVal, firstRate, secondCurrencyVal, secondRate, firstAmountVal, convertedAmount) {
+    document.getElementById('date').innerText = data.time_last_update_utc;
 
-// function manipDOM(data, firstCurrencyVal, firstRate, secondCurrencyVal, secondRate) {
-//     //Change date
-//     document.getElementById('date').innerText = data.time_last_update_utc;
-//     //Change first currency and amount
-//     document.getElementById('first-amount-display').innerText = firstRate;
-//     document.getElementById('first-currency-display').innerText = firstCurrencyVal;
-//     //Change second currency and amount
-//     document.getElementById('second-amount-display').innerText = secondRate;
-//     document.getElementById('second-currency-display').innerText = secondCurrencyVal;
-// }
+    document.getElementById('first-amount-display').innerText = firstAmountVal;
+    document.getElementById('first-currency-display').innerText = firstCurrencyVal;
 
-// //Event listener
-// firstCurrencyEl.addEventListener('change', calculate);
-// firstAmountEl.addEventListener('input', calculate);
-// secondCurrencyEl.addEventListener('change', calculate);
-// secondAmountEl.addEventListener('input', calculate);
+    document.getElementById('second-amount-display').innerText = convertedAmount.toFixed(2);
+    document.getElementById('second-currency-display').innerText = secondCurrencyVal;
+}
 
+firstCurrencyEl.addEventListener('change', calculate);
+firstAmountEl.addEventListener('input', calculate);
+secondCurrencyEl.addEventListener('change', calculate);
 
 calculate();
